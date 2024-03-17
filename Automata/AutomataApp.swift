0ef2faf86d28f4fcdfbaad8cc4419f152b5c2bfb
Nick Twyman <nick@conky.org>
2024-03-16 19:34:04 -0700

    Initial Commit

---
+++ Automata/AutomataApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomataApp.swift
+//  Automata
+//
+//  Created by Nick Twyman on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct AutomataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Automata/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Automata
+//
+//  Created by Nick Twyman on 3/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
